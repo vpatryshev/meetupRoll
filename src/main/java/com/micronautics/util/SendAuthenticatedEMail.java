7f9b999af417a68215051add6ded6ecfe3d15ee9
Mike Slinn <mslinn@mslinn.com>
2012-02-20 23:15:39 -0800

    Now checks date of event

--- src/main/java/com/micronautics/util/SendAuthenticatedEMail.java
+++ src/main/java/com/micronautics/util/SendAuthenticatedEMail.java
@@ -11,99 +11,94 @@ package com.micronautics.util;
  * @author Sudhir Ancha
  * @author Mike Slinn */
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 import java.util.Properties;
 
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 
 public class SendAuthenticatedEMail {
-	private String smtpHost;
-	private String smtpUser;
-	private String smtpPwd;
-	private int smtpPort;
+    private String smtpHost;
+    private String smtpUser;
+    private String smtpPwd;
+    private int smtpPort;
 
 
-	public static void sendEmail(String recipients, String subject, String body, String from) {
-		try {
-			new SendAuthenticatedEMail().sendMail(recipients, subject, body, from);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void sendMail(String recipients, String subject, String body, String from) throws MessagingException {
-		readProps();
+    public static void sendEmail(String recipients, String subject, String body, String from) {
+        try {
+            new SendAuthenticatedEMail().sendMail(recipients, subject, body, from);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 
-		Properties props = new Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable","true");
-		props.put("mail.smtp.port", smtpPort);
+    public void sendMail(String recipients, String subject, String body, String from) throws MessagingException {
+        readProps();
 
-		Authenticator auth = new SMTPAuthenticator();
-		Session session = Session.getDefaultInstance(props, auth);
-		session.setDebug(false);
+        Properties props = new Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", smtpPort);
 
-		Message msg = new MimeMessage(session);
-		final Address[] recipientAddresses = InternetAddress.parse(recipients);
+        Authenticator auth = new SMTPAuthenticator();
+        Session session = Session.getDefaultInstance(props, auth);
+        session.setDebug(false);
+
+        Message msg = new MimeMessage(session);
+        final Address[] recipientAddresses = InternetAddress.parse(recipients);
         msg.setRecipients(Message.RecipientType.TO, recipientAddresses);
         msg.setSentDate(new Date());
         msg.setSubject(subject);
         msg.setText(body);
-		msg.setSubject(subject);
-		msg.setText(body);
+        msg.setSubject(subject);
+        msg.setText(body);
 
-		InternetAddress addressFrom = new InternetAddress(from);
-		msg.setFrom(addressFrom);
+        InternetAddress addressFrom = new InternetAddress(from);
+        msg.setFrom(addressFrom);
 
-		Transport transport = session.getTransport("smtps");
-		try {
-			transport.connect(smtpHost, smtpPort, smtpUser, smtpPwd);
-			transport.sendMessage(msg, recipientAddresses);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		} finally {
-			transport.close();
-		}
-	}
+        Transport transport = session.getTransport("smtps");
+        try {
+            transport.connect(smtpHost, smtpPort, smtpUser, smtpPwd);
+            transport.sendMessage(msg, recipientAddresses);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            transport.close();
+        }
+    }
 
-	private void readProps() {
-		Properties properties = new Properties();
-		InputStream in = getClass().getClassLoader().getResourceAsStream(
-				"meetup.properties");
-		if (in != null) {
-			try {
-				properties.load(in);
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-				return;
-			}
-			smtpHost = properties.getProperty("smtpHost");
-			smtpPort = Integer.parseInt(properties.getProperty("smtpPort"));
-			smtpUser = properties.getProperty("smtpUser");
-			smtpPwd = properties.getProperty("smtpPwd");
-		}
-	}
+    private void readProps() {
+        Properties properties = new Properties();
+        InputStream in = getClass().getClassLoader().getResourceAsStream(
+                "meetup.properties");
+        if (in != null) {
+            try {
+                properties.load(in);
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            smtpHost = properties.getProperty("smtpHost");
+            smtpPort = Integer.parseInt(properties.getProperty("smtpPort"));
+            smtpUser = properties.getProperty("smtpUser");
+            smtpPwd = properties.getProperty("smtpPwd");
+        }
+    }
 
-	/** Does simple authentication when the SMTP server requires it. */
-	private class SMTPAuthenticator extends javax.mail.Authenticator {
+    /**
+     * Does simple authentication when the SMTP server requires it.
+     */
+    private class SMTPAuthenticator extends javax.mail.Authenticator {
 
-		public PasswordAuthentication getPasswordAuthentication() {
-			String username = smtpUser;
-			String password = smtpPwd;
-			return new PasswordAuthentication(username, password);
-		}
-	}
+        public PasswordAuthentication getPasswordAuthentication() {
+            String username = smtpUser;
+            String password = smtpPwd;
+            return new PasswordAuthentication(username, password);
+        }
+    }
 }
--- src/main/scala/com/micronautics/meetupRoll/MeetupRoll.scala
+++ src/main/scala/com/micronautics/meetupRoll/MeetupRoll.scala
@@ -20,47 +20,48 @@ object MeetupRoll extends App {
   private val Names = """<span class="D_name">(\S+) (\S*)""".r
   private val properties = readProps
   private val meetupGroup = Option(properties.getProperty("meetupGroup")).orElse(Some("Bay-Area-Scala-Enthusiasts")).get
-  private val eventId     = Option(properties.getProperty("eventId"))    .orElse(Some("44582312")).get
-  
+  private val eventId = Option(properties.getProperty("eventId")).orElse(Some("44582312")).get
+
   private def groupUrl = "http://www.meetup.com/" + meetupGroup + "/events/" + eventId + "/printrsvp"
+
   private val attendeesPage = new URL(groupUrl).asInput.slurpString(Codec.UTF8)
   private val title = (Title findFirstMatchIn attendeesPage) match {
-    case Some(x) => x group(1)
-    case None => ""
-  }
-  
-  private val date = (Date findFirstMatchIn attendeesPage) match {
-    case Some(x) => x group(1)
+    case Some(x) => x group (1)
     case None => ""
   }
 
-  /** Mutable list of full names. If a member did not specify a last name they will not appear in the list.
-    * Names that are chosen are removed so they cannot be chosen again. */
+  private val date = (Date findFirstMatchIn attendeesPage) match {
+    case Some(x) => x group (1)
+    case None => ""
+  }
+
+  /**Mutable list of full names. If a member did not specify a last name they will not appear in the list.
+   * Names that are chosen are removed so they cannot be chosen again. */
   private var names = (for (m <- (Names findAllIn attendeesPage).matchData)
-    yield m.group(1) + " " + m.group(2)).toList.toBuffer
+  yield m.group(1) + " " + m.group(2)).toList.toBuffer
 
   private def numNames = names.length
-  
+
   private def randomName = names(random.nextInt(numNames))
-  
+
   private def readProps = {
     val properties = new Properties()
     val in = MeetupRoll.getClass().getClassLoader().getResourceAsStream("meetup.properties")
-    if (in==null) {
-    	System.err.println("Could not read meetup.properties, aborting.");
-    	System.exit(1);
+    if (in == null) {
+      System.err.println("Could not read meetup.properties, aborting.");
+      System.exit(1);
     } else {
       properties.load(in)
       in.close()
     }
     properties
   }
-  
-  private def isEventToday(date:String):Boolean = {
-    val eventDate:Date = try {
+
+  private def isEventToday(date: String): Boolean = {
+    val eventDate: Date = try {
       new SimpleDateFormat("EEEE, MMMMM dd, yyyy HH:mm a").parse(date)
     } catch {
-      case pe:ParseException =>
+      case pe: ParseException =>
         println(pe)
         System.exit(1)
         new Date()
@@ -73,30 +74,30 @@ object MeetupRoll extends App {
       cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR)
     sameDay
   }
-    
+
   val aPage = attendeesPage
-  var winners = new ListMap[String,String].empty
+  var winners = new ListMap[String, String].empty
   println("Parsed " + names.length + " names from \"" + title + "\"")
   println("Scheduled for " + date)
   println(groupUrl)
   if (!isEventToday(date))
     println("\n*** THIS EVENT IS NOT HELD TODAY ***\n")
   while (true) {
-      val name = randomName
-      names -= name
-      println("Winner: " + name)
-      val token = Console.readLine("Type the name of the prize " + name + " won, or type Enter to exit > ")
-      if (token=="") {
-        if (winners.size>0) {
-          var winString = "Winners are:\n";
-          for (winner <- winners)
-            winString += "  " + winner._1 + ": " + winner._2 + "\n"
-          println(winString + "\nSending email so you remember...")
-          SendAuthenticatedEMail.sendEmail(properties.getProperty("smtpUser"), "Giveaway winners", winString, properties.getProperty("smtpUser"))
-        }
-        println("Done.")
-        System.exit(0)
+    val name = randomName
+    names -= name
+    println("Winner: " + name)
+    val token = Console.readLine("Type the name of the prize " + name + " won, or type Enter to exit > ")
+    if (token == "") {
+      if (winners.size > 0) {
+        var winString = "Winners are:\n";
+        for (winner <- winners)
+          winString += "  " + winner._1 + ": " + winner._2 + "\n"
+        println(winString + "\nSending email so you remember...")
+        SendAuthenticatedEMail.sendEmail(properties.getProperty("smtpUser"), "Giveaway winners", winString, properties.getProperty("smtpUser"))
       }
-      winners += name -> token
+      println("Done.")
+      System.exit(0)
     }
+    winners += name -> token
+  }
 }
