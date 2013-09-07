f7e70e742dd40a508082c7cd84b88691eae59543
Julia Astakhova <jastakhova@griddynamics.com>
2013-09-07 15:59:41 -0700

    cleaned

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -11,30 +11,10 @@ import Loc._
 import sun.nio.cs.StandardCharsets
 import org.apache.commons.codec.CharEncoding
 
-
-/**
- * A class that's instantiated early and run.  It allows the application
- * to modify lift's environment
- */
 class Boot {
   def boot {
-    // where to search snippet
     LiftRules.addToPackages("com.micronautics.meetupRoll.web")
 
-    // Build SiteMap
-//    val entries = Menu(Loc("Home", List("index"), "Home")) :: Nil
-
-    // set the sitemap.  Note if you don't want access control for
-    // each page, just comment this line out.
-//    LiftRules.setSiteMap(SiteMap(entries:_*))
-
-//    LiftRules.ajaxStart =
-//      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-//
-//    LiftRules.ajaxEnd =
-//      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding(CharEncoding.UTF_8))
 
     LiftRules.siteMapFailRedirectLocation = List("/")
